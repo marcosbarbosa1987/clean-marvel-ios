3f8e9371ea4fae8c9d633970c56ab3d3ef8c22d2
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-10-19 23:42:45 -0300

    Create infra layer;

---
+++ clear-marvel/InfraTests/AlamofireAdaptarTest.swift
@@ -0,0 +1,25 @@
+//
+//  AlamofireAdapterTest.swift
+//  InfraTests
+//
+//  Created by Marcos Barbosa on 19/10/21.
+//
+
+import XCTest
+import Data
+
+class AlamofireAdapterTest: XCTestCase {
+    
+    func test_() {
+        let sut = AlamofireAdapter()
+        sut.get()
+    }
+}
+
+
+class AlamofireAdapter {
+    
+    func get() {
+        
+    }
+}
